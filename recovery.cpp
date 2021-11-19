983d14edc4203c296818d5a11c4aebc53b6aae11
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2021-11-19 12:17:18 +0330

    recovery: Use props ro show verion and build date
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I52a79e2e0e93e728a7949fe02da67e8dc527daf7

--- recovery.cpp
+++ recovery.cpp
@@ -750,17 +750,9 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
     ui->SetStage(st_cur, st_max);
   }
 
-  // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
-  // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
-  // LMODROID_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.lmodroid.version", "");
-  std::smatch ver_date_match;
-  std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
-  std::string ver_date = ver_date_match.str(1);  // Empty if no match.
-
+  // Using props to show LMODroid Verion and Build date.
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lmodroid.build_date", "(unknown)") +
-        " (" + ver_date + ")",
+    "Build date: " + android::base::GetProperty("ro.lmodroid.build_date", "(unknown)") + " - " +"Version: " + android::base::GetProperty("ro.lmodroid.version", "(unknown)")
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");
